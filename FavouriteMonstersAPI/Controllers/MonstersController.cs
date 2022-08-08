b9bf28dfd36b8f5927c6250d24c7cbe398d203d0
Anthony Lee <anthonyleehj@gmail.com>
2022-08-08 17:36:26 +0800

    Created UI for displaying monster teams

--- FavouriteMonstersAPI/Controllers/MonstersController.cs
+++ FavouriteMonstersAPI/Controllers/MonstersController.cs
@@ -6,78 +6,78 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FavouriteMonstersAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MonstersController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class MonstersController : ControllerBase
+  {
+    private readonly MonstersDbContext _context;
+
+    public MonstersController(MonstersDbContext context)
     {
-        private readonly MonstersDbContext _context;
-
-        public MonstersController(MonstersDbContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            var mons = await (from monsters in _context.Monsters
-                              join elements in _context.Elements on monsters.ElementId equals elements.Id
-                              select new
-                              {
-                                  Id = monsters.Id,
-                                  Name = monsters.Name,
-                                  Element = elements.Name,
-                                  Color = elements.Color,
-                                  Url = monsters.ImageUrl
-                              })
-                                    .AsNoTracking()
-                                    .ToListAsync();
-
-            return Ok(mons);
-        }
-
-        [HttpGet("{id}")]
-        [ProducesResponseType(typeof(Monsters), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetById(Guid id)
-        {
-            var monster = await _context.Monsters.FindAsync(id);
-            return monster == null ? NotFound() : Ok(monster);
-        }
-
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        public async Task<IActionResult> Create([FromBody] Monsters monster)
-        {
-            await _context.Monsters.AddAsync(monster);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetById), new { id = monster.Id }, monster);
-        }
-
-        [HttpPut("{id}")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Update(Guid id, [FromBody] Monsters monster)
-        {
-            if (id != monster.Id) return BadRequest();
-
-            _context.Entry(monster).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            var monsterToDelete = await _context.Monsters.FindAsync(id);
-            if (monsterToDelete == null) return NotFound();
-
-            _context.Monsters.Remove(monsterToDelete);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
+      _context = context;
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+      var monsterList = await (from monsters in _context.Monsters
+                               join elements in _context.Elements on monsters.ElementId equals elements.Id
+                               select new
+                               {
+                                 Id = monsters.Id,
+                                 Name = monsters.Name,
+                                 Element = elements.Name,
+                                 Color = elements.Color,
+                                 Url = monsters.ImageUrl
+                               })
+                              .AsNoTracking()
+                              .ToListAsync();
+
+      return Ok(monsterList);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(Monsters), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+      var monster = await _context.Monsters.FindAsync(id);
+      return monster == null ? NotFound() : Ok(monster);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<IActionResult> Create([FromBody] Monsters monster)
+    {
+      await _context.Monsters.AddAsync(monster);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetById), new { id = monster.Id }, monster);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Update(Guid id, [FromBody] Monsters monster)
+    {
+      if (id != monster.Id) return BadRequest();
+
+      _context.Entry(monster).State = EntityState.Modified;
+      await _context.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+      var monsterToDelete = await _context.Monsters.FindAsync(id);
+      if (monsterToDelete == null) return NotFound();
+
+      _context.Monsters.Remove(monsterToDelete);
+      await _context.SaveChangesAsync();
+
+      return NoContent();
+    }
+  }
 }
