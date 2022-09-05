9e61f05102f19a87303f905221343dfacc9ff320
Anthony Lee <anthonyleehj@gmail.com>
2022-09-05 18:01:39 +0800

    Removed votes, added monster stats

--- FavouriteMonstersAPI/Models/Monsters.cs
+++ FavouriteMonstersAPI/Models/Monsters.cs
@@ -11,6 +11,13 @@ namespace FavouriteMonstersAPI.Models
     [Required]
     public Guid ElementId { get; set; }
     public string ImageUrl { get; set; }
-    public int Votes { get; set; } = 0;
+    [Range(1, 255, ErrorMessage = "Value must be between 1 and 255")]
+    public int HP { get; set; }
+    [Range(1, 255, ErrorMessage = "Value must be between 1 and 255")]
+    public int Attack { get; set; }
+    [Range(1, 255, ErrorMessage = "Value must be between 1 and 255")]
+    public int Defence { get; set; }
+    [Range(1, 255, ErrorMessage = "Value must be between 1 and 255")]
+    public int Speed { get; set; }
   }
 }
