b016f3f823d9478f48b86e402fb161c368974345
Anthony Lee <anthonyleehj@gmail.com>
2022-09-06 11:04:27 +0800

    Added swagger support to production

--- FavouriteMonstersAPI/Program.cs
+++ FavouriteMonstersAPI/Program.cs
@@ -52,11 +52,8 @@ builder.Services.AddCors(policy =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("OpenCorsPolicy");
